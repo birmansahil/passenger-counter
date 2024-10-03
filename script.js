68dbdfefc045800816722680f22b5c8aa37bea67
birmansahil <contact.birmansahil@gmail.com>
2024-10-02 23:44:43 -0400

    added styles

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 let count = 0;
 
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
+}
+
 function increment() {
     count += 1;
     countEl.textContent = count;
